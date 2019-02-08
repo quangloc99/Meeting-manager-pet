ec6b1fedcf8a489417f32b1f30619bd951661e14
Tran Quang Loc <quangloctran99@gmail.com>
2019-02-08 09:42:37 +0300

    Add CSV parser.

--- src/ru/ifmo/se/s267880/lab5/Main.java
+++ src/ru/ifmo/se/s267880/lab5/Main.java
@@ -1,27 +1,28 @@
 package ru.ifmo.se.s267880.lab5;
 
-import ru.ifmo.se.s267880.lab5.cvs.CsvLexerStateMachine;
+import ru.ifmo.se.s267880.lab5.csv.CsvParser;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        CsvLexerStateMachine sm = new CsvLexerStateMachine() {
-            @Override
-            public void whenGotNewField(String field) {
-                System.out.println("field:" + field);
-            }
-
-            @Override
-            public void whenRowEnd() {
-                System.out.println("end of row");
-            }
-        };
         String csv = "name,\"high score\",time\r\n" +
                 "\"Tran,Quang,Loc\",1000,Dec 2019\r\n" +
-                "\"Nguyen\"\" Vinh\"\"Thinh\", 2000 , Jan 2020";
-        System.out.println(csv);
-        for (char x: csv.toCharArray()) {
-            sm.feed(x);
+                "\"Nguyen\"\" Vinh\"\"Thinh\", 2000 , Jan 2020\r\n" +
+                ",,\r\n" +
+                "cool,,yeah";
+        try {
+            CsvParser ps = new CsvParser(new ByteArrayInputStream(csv.getBytes()), true);
+            Map<String, String> row;
+            do {
+                row = ps.getNextRowWithNames();
+                System.out.println(row);
+            } while (row != null);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        sm.feed(-1);
     }
 }
