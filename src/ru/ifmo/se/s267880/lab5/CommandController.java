e77a9ea9bd716481ee4b630d7b8aecd87cc0e565
Tran Quang Loc <quangloctran99@gmail.com>
2019-02-09 11:29:46 +0300

    Add CommandController class.

---
+++ src/ru/ifmo/se/s267880/lab5/CommandController.java
@@ -0,0 +1,96 @@
+package ru.ifmo.se.s267880.lab5;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CommandController {
+    public interface Handler {}
+    public interface HandlerWithNoArgument extends Handler {
+        void process();
+    }
+
+    public interface HandlerWithJson extends Handler {
+        void process(JsonElement json);
+    }
+
+    public class CommandNotFoundException extends Exception {
+        public CommandNotFoundException(String commandName) {
+            super("Command \"" + commandName + "\" not found. Use command \"list-commands\" for the list of usable commands.");
+        }
+    }
+
+    private Map<String, Handler> commandHandlers = new TreeMap<>();  // so the commands can be alphabetized
+    private Map<String, String> commandUsages = new HashMap<>();
+
+    protected InputStreamReader userInputStream;
+
+    public CommandController(InputStream userInputStream) {
+        this.userInputStream = new InputStreamReader(userInputStream);
+        addCommand("list-commands", "list all the commands", () -> {
+            System.out.println("Commands list:");
+            commandHandlers.forEach((commandName, handler) -> System.out.printf("   - %15s - %s\n",
+                commandName + (handler instanceof HandlerWithJson ? " {input}" : ""),
+                commandUsages.get(commandName)
+            ));
+        });
+    }
+
+    public CommandController() {
+        this(System.in);
+    }
+
+    public void addCommand(String commandName, String usage, HandlerWithNoArgument handler) {
+        commandUsages.put(commandName, usage);
+        commandHandlers.put(commandName, handler);
+    }
+
+    public void addCommandWithJson(String commandName, String usage, HandlerWithJson handler) {
+        commandUsages.put(commandName, usage);
+        commandHandlers.put(commandName, handler);
+    }
+
+    public Handler getCommandHandler(String commandName) {
+        return commandHandlers.get(commandName);
+    }
+
+    public String getCommandUsage(String commandName) {
+        return commandUsages.get(commandName);
+    }
+
+    public void prompt() throws IOException, CommandNotFoundException {
+        System.out.printf("> ");
+        String userCommand = getUserCommand();
+        if (!commandHandlers.containsKey(userCommand)) {
+            throw new CommandNotFoundException(userCommand);
+        }
+        Handler handler = commandHandlers.get(userCommand);
+        if (handler instanceof HandlerWithJson) {
+            ((HandlerWithJson)handler).process(getUserJsonInput());
+        } else {
+            ((HandlerWithNoArgument)handler).process();
+        }
+    }
+
+    protected String getUserCommand() throws IOException {
+        int currentByte;
+        while ((currentByte = userInputStream.read()) == ' ');
+        String command = "" + (char) currentByte;
+        while (true) {
+            currentByte = userInputStream.read();
+            if (Character.isWhitespace(currentByte) || currentByte == -1) break;
+            command += (char) currentByte;
+        }
+        return command;
+    }
+
+    protected JsonElement getUserJsonInput() {
+        return (new JsonParser()).parse(userInputStream);
+    }
+}
--- src/ru/ifmo/se/s267880/lab5/Main.java
+++ src/ru/ifmo/se/s267880/lab5/Main.java
@@ -1,33 +1,51 @@
 package ru.ifmo.se.s267880.lab5;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 import ru.ifmo.se.s267880.lab5.csv.CsvParser;
 import ru.ifmo.se.s267880.lab5.csv.CsvRowWriter;
 import ru.ifmo.se.s267880.lab5.csv.CsvRowWithNamesWriter;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.StringReader;
+import java.rmi.server.ExportException;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
-        String csv = "name,\"high score\",time\r\n" +
-                "\"Tran Quang Loc\",1000,Dec 2019\r\n" +
-                "\"Nguyen\"\" Vinh\"\"Thinh\", 2000 , Jan 2020\r\n" +
-                ",,\r\n" +
-                "cool,,yeah";
-        try {
-            CsvParser ps = new CsvParser(new ByteArrayInputStream(csv.getBytes()), true);
-            CsvRowWithNamesWriter rw = new CsvRowWithNamesWriter(System.out, List.of("high score", "time", "name"));
-            Map<String, String> row;
-//            List<String> row;
-            while (true) {
-                row = ps.getNextRowWithNames();
-                if (row == null) break;
-                rw.writeRow(row);
+//        String csv = "name,\"high score\",time\r\n" +
+//                "\"Tran Quang Loc\",1000,Dec 2019\r\n" +
+//                "\"Nguyen\"\" Vinh\"\"Thinh\", 2000 , Jan 2020\r\n" +
+//                ",,\r\n" +
+//                "cool,,yeah";
+//        String json = "{\n" +
+//                "\"name\": \"abc xyz\"\n," +
+//                "\"age\": 100,\n" +
+//                "\"films\": [\n" +
+//                "   \"adventure time\",\n" +
+//                "   \"svtfoe\"\n" +
+//                "]\n" +
+//                "}\n [1, 2, 3, 4]";
+//        InputStreamReader msc = new InputStreamReader(System.in);
+//        StringReader sr = new StringReader(json) ;
+//        JsonReader jreader = new JsonReader(sr);
+////        jreader.setLenient(false);
+//        JsonElement je = (new JsonParser()).parse(jreader);
+//        System.out.println(json.length());
+//        System.out.printf("%20s", je.getAsJsonObject().get("name").getAsString());
+        CommandController cc = new CommandController();
+        cc.addCommand("exit", "escape the program", () -> System.exit(0));
+        while (true) {
+            try {
+                cc.prompt();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
