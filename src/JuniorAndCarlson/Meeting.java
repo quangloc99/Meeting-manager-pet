4e470851b0335423dcefd740abbfe89b800ad038
Tran Quang Loc <quangloctran99@gmail.com>
2019-02-09 22:23:53 +0300

    Add standard functions into Meeting.

--- src/JuniorAndCarlson/Meeting.java
+++ src/JuniorAndCarlson/Meeting.java
@@ -1,7 +1,7 @@
 package JuniorAndCarlson;
 import java.util.Date;
 
-public class Meeting {
+public class Meeting implements Comparable<Meeting> {
     public class LateException extends RuntimeException {
         public LateException() {
             super("Встреча " + meetingName + " была отменена из-за опоздания");
@@ -30,5 +30,26 @@ public class Meeting {
     public Date getTime() {
         return (Date) meetingTime.clone();
     }
+
+    @Override
+    public String toString() {
+        return "Meeting \"" + meetingName + "\" at " + meetingTime;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) return false;
+        if (other == this) return true;
+        if (other.getClass() != this.getClass()) return false;
+
+        Meeting o = (Meeting) other;
+        return o.meetingTime.equals(this.meetingTime) && o.meetingName.equals(this.meetingName);
+    }
+
+    @Override
+    public int compareTo(Meeting other) {
+        return meetingTime.compareTo(other.meetingTime);
+    }
+
 }
 
