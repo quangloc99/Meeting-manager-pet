533b28dd556e4d9d3da378ecb738f871603367a6
Tran Quang Loc <quangloctran99@gmail.com>
2019-02-09 15:15:44 +0300

    Add InputPreprocessor.

---
+++ src/ru/ifmo/se/s267880/lab5/commandControllerHelper/InputPreprocessor.java
@@ -0,0 +1,15 @@
+package ru.ifmo.se.s267880.lab5.commandControllerHelper;
+
+import com.google.gson.JsonElement;
+
+public class InputPreprocessor {
+    public Object preprocess(JsonElement elm, Class inputType) {
+        if (inputType == Integer.class) {
+            return elm.getAsInt();
+        }
+        if (inputType == String.class) {
+            return elm.getAsString();
+        }
+        return elm;
+    }
+}
--- src/ru/ifmo/se/s267880/lab5/commandControllerHelper/ReflectionCommandAdder.java
+++ src/ru/ifmo/se/s267880/lab5/commandControllerHelper/ReflectionCommandAdder.java
@@ -3,9 +3,10 @@ package ru.ifmo.se.s267880.lab5.commandControllerHelper;
 import ru.ifmo.se.s267880.lab5.CommandController;
 
 import java.lang.reflect.Method;
+import java.util.Map;
 
 public class ReflectionCommandAdder {
-    public static void addCommand(CommandController cc, Object obj) {
+    public static void addCommand(CommandController cc, Object obj, InputPreprocessor preprocessor) {
         Class cls = obj.getClass();
         for (Method med: cls.getDeclaredMethods()) {
             Command anno = med.getAnnotation(Command.class);
@@ -13,9 +14,10 @@ public class ReflectionCommandAdder {
             if (anno == null) continue;
             med.setAccessible(true);           // This line is necessary, even when the method is public.
             if (med.getParameterCount() > 0) {
+                Class inputType = med.getParameterTypes()[0];
                 cc.addCommandWithJson(med.getName(), anno.usage(), elm -> {
                     try {
-                        med.invoke(obj, elm);
+                        med.invoke(obj, preprocessor.preprocess(elm, inputType));
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
