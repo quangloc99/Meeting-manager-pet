19dcc438995073b696028735b60dc38fe227bb25
Tran Quang Loc <quangloctran99@gmail.com>
2019-02-09 22:27:28 +0300

    Minor changes for InputPreprocessor.
    - Add Integer.TYPE of the inputType is primitive.
    - Return null instead of keeping the if no type are matched.

--- src/ru/ifmo/se/s267880/lab5/commandControllerHelper/InputPreprocessor.java
+++ src/ru/ifmo/se/s267880/lab5/commandControllerHelper/InputPreprocessor.java
@@ -4,12 +4,12 @@ import com.google.gson.JsonElement;
 
 public class InputPreprocessor {
     public Object preprocess(JsonElement elm, Class inputType) {
-        if (inputType == Integer.class) {
+        if (inputType == Integer.class || inputType == Integer.TYPE) {
             return elm.getAsInt();
         }
         if (inputType == String.class) {
             return elm.getAsString();
         }
-        return elm;
+        return null;
     }
 }
