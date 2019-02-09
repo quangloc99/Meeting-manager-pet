73f9e5455b471fec696c6b3a55938e3450cf5095
Tran Quang Loc <quangloctran99@gmail.com>
2019-02-09 22:28:11 +0300

    Add options for overload commands.

--- src/ru/ifmo/se/s267880/lab5/commandControllerHelper/ReflectionCommandAdder.java
+++ src/ru/ifmo/se/s267880/lab5/commandControllerHelper/ReflectionCommandAdder.java
@@ -3,34 +3,57 @@ package ru.ifmo.se.s267880.lab5.commandControllerHelper;
 import ru.ifmo.se.s267880.lab5.CommandController;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class ReflectionCommandAdder {
     public static void addCommand(CommandController cc, Object obj, InputPreprocessor preprocessor) {
         Class cls = obj.getClass();
+        Map<String, LinkedList<Method>> commandList = new HashMap<>();
         for (Method med: cls.getDeclaredMethods()) {
             Command anno = med.getAnnotation(Command.class);
-            System.out.println(med.getName());
             if (anno == null) continue;
+
             med.setAccessible(true);           // This line is necessary, even when the method is public.
-            if (med.getParameterCount() > 0) {
-                Class inputType = med.getParameterTypes()[0];
-                cc.addCommandWithJson(med.getName(), anno.usage(), elm -> {
-                    try {
-                        med.invoke(obj, preprocessor.preprocess(elm, inputType));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });
-            } else {
-                cc.addCommand(med.getName(), anno.usage(), () -> {
-                    try {
-                        med.invoke(obj);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+
+            String commandName = anno.commandName();
+            if (commandName.equals("")) {
+                commandName = med.getName();
+            }
+
+            if (!commandList.containsKey(commandName)) {
+                commandList.put(commandName, new LinkedList<>());
+            }
+
+            commandList.get(commandName).add(med);
+        }
+
+        commandList.forEach((commandName, methodList) -> {
+            boolean hasOtherParam = false;
+            List<String> commandWithParamUsage = new LinkedList<>();
+            for (Method med: methodList) {
+                Command anno = med.getAnnotation(Command.class);
+                if (med.getParameterCount() > 0) {
+                    hasOtherParam = true;
+                    commandWithParamUsage.add(anno.usage());
+                    continue;
+                }
+                cc.addCommand(commandName, anno.usage(), () -> {
+                    med.invoke(obj);
                 });
             }
-        }
+            if (!hasOtherParam) return;
+            cc.addCommandWithJson(commandName, String.join("\n", commandWithParamUsage), elm -> {
+                for (Method med: methodList) {
+                    Class inputType = med.getParameterTypes()[0];
+                    Object inp = preprocessor.preprocess(elm, inputType);
+                    if (inp == null) continue;
+                    med.invoke(obj, inp);
+                    break;
+                }
+            });
+        });
     }
 }
