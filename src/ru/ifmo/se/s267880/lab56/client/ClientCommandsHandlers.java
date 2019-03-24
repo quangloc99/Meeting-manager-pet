3b44d152f3b4227501cb570fc4d825d6dcc9b8dd
Tran Quang Loc <quangloctran99@gmail.com>
2019-03-25 01:32:21 +0300

    Add CommandExecutor.

--- src/ru/ifmo/se/s267880/lab56/client/ClientCommandsHandlers.java
+++ src/ru/ifmo/se/s267880/lab56/client/ClientCommandsHandlers.java
@@ -1,27 +1,61 @@
 package ru.ifmo.se.s267880.lab56.client;
 
 import ru.ifmo.se.s267880.lab56.shared.*;
+import ru.ifmo.se.s267880.lab56.shared.commandsController.helper.Command;
 
-import javax.xml.transform.Result;
 import java.io.*;
 import java.nio.ByteBuffer;
-import java.nio.channels.Channel;
 import java.nio.channels.Channels;
 import java.nio.channels.SocketChannel;
-import java.nio.channels.WritableByteChannel;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
 import java.util.stream.IntStream;
-
-interface DataWriterToSocket {
-    void writeData(SocketChannel channel) throws IOException;
-}
 
 abstract public class ClientCommandsHandlers implements CommandHandlersWithMeeting {
     private String currentCommandName = null;
     private Object[] currentCommandParams;
+
+    private class CommandExecutor {
+        public CommandExecutor() {}
+
+        public ResultToClient run() throws Exception {
+            try (SocketChannel channel = createChannel()) {
+                sendData(channel, generateQuery());
+                channel.shutdownOutput();
+                ResultToClient res = receiveData(channel);
+                processResult(res);
+                return res;
+            } catch (EOFException | ClassNotFoundException e) {
+                throw new IOException("Result sent from server has wrong format or there is a problem with connection.", e);
+            }
+        }
+
+        protected QueryToServer generateQuery() throws Exception {
+            Serializable[] castedParams = new Serializable[currentCommandParams.length];
+            for (int i = 0; i < currentCommandParams.length; ++i) {
+                assert (currentCommandParams[i] instanceof Serializable);
+                castedParams[i] = (Serializable) currentCommandParams[i];
+            }
+            return new QueryToServer(currentCommandName, castedParams);
+        }
+
+        protected void sendData(SocketChannel channel, QueryToServer qr) throws Exception {
+            ByteBuffer bf = ByteBuffer.wrap(Helper.serializableToByteArray(qr));
+            channel.write(bf);
+        }
+
+        protected ResultToClient receiveData(SocketChannel channel) throws Exception {
+            ObjectInputStream in = new ObjectInputStream(Channels.newInputStream(channel));
+            return (ResultToClient) in.readObject();
+        }
+
+        protected void processResult(ResultToClient res) throws Exception {
+            if (res.getStatus() == ResultToClientStatus.FAIL) {
+                throw (Exception) res.getResult();
+            }
+        }
+    }
 
     /**
      * Set current command information.
@@ -35,67 +69,35 @@ abstract public class ClientCommandsHandlers implements CommandHandlersWithMeeti
 
     abstract public SocketChannel createChannel() throws IOException;
 
-    ResultToClient queryToServer(DataWriterToSocket dataWriter) throws Exception {
-        try (SocketChannel sc = createChannel()) {
-            dataWriter.writeData(sc);
-            ObjectInputStream in = new ObjectInputStream(Channels.newInputStream(sc));
-            ResultToClient res = (ResultToClient) in.readObject();
-            if (res.getStatus() == ResultToClientStatus.FAIL) {
-                throw (Exception) res.getResult();
-            }
-            return res;
-        } catch (EOFException | ClassNotFoundException e) {
-            throw new IOException("Result sent from server has wrong format or there is a problem with connection.", e);
-        }
-    }
-
-    private QueryToServer generateQuery() {
-        Serializable[] castedParams = new Serializable[currentCommandParams.length];
-        for (int i = 0; i < currentCommandParams.length; ++i) {
-            assert (currentCommandParams[i] instanceof Serializable);
-            castedParams[i] = (Serializable) currentCommandParams[i];
-        }
-
-        return new QueryToServer(currentCommandName, castedParams);
-    }
-
-    ResultToClient queryToServer() throws Exception {
-        return queryToServer(socketChannel -> {
-            ObjectOutputStream out = new ObjectOutputStream(Channels.newOutputStream(socketChannel));
-
-//            ByteBuffer bf = ByteBuffer.wrap(Helper.serializableToByteArray(qr));
-//            sc.write(bf);
-            out.writeObject(generateQuery());
-            socketChannel.shutdownOutput();
-        });
-    }
-
     /**
      * Add all data from another file into the current collection.
      * @param inputStream the input stream that the data will be imported from.
      */
     @Override
     public void doImport(InputStream inputStream) throws Exception {
-        queryToServer(channel -> {
-            ByteBuffer queryBuffer = ByteBuffer.wrap(Helper.serializableToByteArray(
-                    new QueryToServer(currentCommandName, new Serializable[]{
-                            null    // because there is a least 1 element we cannot sent InputStream to server.
-                                    // the ServerInputPreprocessor will get the data from Socket and pass to the
-                                    // command's handler on server.
-                    })
-            ));
-            channel.write(queryBuffer);
-            ByteBuffer dataBuffer = ByteBuffer.wrap(new byte[1024]);
-            byte[] data = new byte[1024];
-            int numRead;
-            while ((numRead = inputStream.read(data)) != -1) {
-                dataBuffer.clear();
-                dataBuffer.put(data, 0, numRead);
-                dataBuffer.flip();
-                channel.write(dataBuffer);
+        new CommandExecutor() {
+            @Override
+            protected QueryToServer generateQuery() {
+                // because there is a least 1 element we cannot sent InputStream to server.
+                // the ServerInputPreprocessor will get the data from Socket and pass to the
+                // command's handler on server.
+                return new QueryToServer(currentCommandName, new Serializable[]{null});
             }
-            channel.shutdownOutput();
-        });
+
+            @Override
+            protected void sendData(SocketChannel channel, QueryToServer qr) throws Exception {
+                super.sendData(channel, qr);
+                ByteBuffer dataBuffer = ByteBuffer.wrap(new byte[1024]);
+                byte[] data = new byte[1024];
+                int numRead;
+                while ((numRead = inputStream.read(data)) != -1) {
+                    dataBuffer.clear();
+                    dataBuffer.put(data, 0, numRead);
+                    dataBuffer.flip();
+                    channel.write(dataBuffer);
+                }
+            }
+        }.run();
     }
 
     /**
@@ -104,17 +106,17 @@ abstract public class ClientCommandsHandlers implements CommandHandlersWithMeeti
      */
     @Override
     public void load(String path) throws Exception {
-        queryToServer();
+        new CommandExecutor().run();
     }
 
     @Override
     public void save() throws Exception {
-        queryToServer();
+        new CommandExecutor().run();
     }
 
     @Override
     public void saveAs(String path) throws Exception {
-        queryToServer();
+        new CommandExecutor().run();
     }
 
     /**
@@ -123,7 +125,7 @@ abstract public class ClientCommandsHandlers implements CommandHandlersWithMeeti
      */
     @Override
     public void add(Meeting meeting) throws Exception {
-        queryToServer();
+        new CommandExecutor().run();
     }
 
     /**
@@ -131,15 +133,19 @@ abstract public class ClientCommandsHandlers implements CommandHandlersWithMeeti
      */
     @Override
     public List<Meeting> show() throws Exception {
-        ResultToClient res= queryToServer();
-        if (res.getStatus() != ResultToClientStatus.SUCCESS) return null;
-        List<Meeting> meetings = (List<Meeting>) res.getResult();
-        System.out.println("# Meeting list:");
-        Iterator<Integer> counter = IntStream.rangeClosed(1, meetings.size()).iterator();
-        meetings.stream()
-                .map(meeting -> String.format("%3d) %s", counter.next(), meeting))
-                .forEachOrdered(System.out::println);
-        return meetings;
+        return (List<Meeting>) (new CommandExecutor() {
+            @Override
+            protected void processResult(ResultToClient res) throws Exception {
+//                super.processResult(res);
+                assert(res.getStatus() == ResultToClientStatus.SUCCESS);
+                List<Meeting> meetings = (List<Meeting>) res.getResult();
+                System.out.println("# Meeting list:");
+                Iterator<Integer> counter = IntStream.rangeClosed(1, meetings.size()).iterator();
+                meetings.stream()
+                    .map(meeting -> String.format("%3d) %s", counter.next(), meeting))
+                    .forEachOrdered(System.out::println);
+            }
+        }.run().getResult());
     }
 
     /**
@@ -148,7 +154,7 @@ abstract public class ClientCommandsHandlers implements CommandHandlersWithMeeti
      */
     @Override
     public void remove(Meeting meeting) throws Exception {
-        queryToServer();
+        new CommandExecutor().run();
     }
 
     /**
@@ -157,7 +163,7 @@ abstract public class ClientCommandsHandlers implements CommandHandlersWithMeeti
      */
     @Override
     public void remove(int num) throws Exception {
-        queryToServer();
+        new CommandExecutor().run();
     }
 
     /**
@@ -166,7 +172,7 @@ abstract public class ClientCommandsHandlers implements CommandHandlersWithMeeti
      */
     @Override
     public void addIfMin(Meeting meeting) throws Exception {
-        queryToServer();
+        new CommandExecutor().run();
     }
 
     /**
@@ -174,12 +180,17 @@ abstract public class ClientCommandsHandlers implements CommandHandlersWithMeeti
      */
     @Override
     public Map<String, String> info() throws Exception {
-        Map<String, String> result = (Map<String, String>) queryToServer().getResult();
-        System.out.println("# Information");
-        System.out.println("File name: " + (result.get("file") == null ? "<<no name>>" : result.get("file")));
-        System.out.println("Number of meeting: " + result.get("meeting-count"));
-        System.out.println("File load since: " + result.get("since"));
-        return result;
+        return (Map<String, String>) new CommandExecutor() {
+            @Override
+            protected void processResult(ResultToClient res) {
+                assert(res.getStatus() == ResultToClientStatus.SUCCESS);
+                Map<String, String> result = (Map<String, String>) res.getResult();
+                System.out.println("# Information");
+                System.out.println("File name: " + (result.get("file") == null ? "<<no name>>" : result.get("file")));
+                System.out.println("Number of meeting: " + result.get("meeting-count"));
+                System.out.println("File load since: " + result.get("since"));
+            }
+        }.run().getResult();
     }
 
     /**
@@ -187,12 +198,12 @@ abstract public class ClientCommandsHandlers implements CommandHandlersWithMeeti
      */
     @Override
     public void sortByDate() throws Exception {
-        queryToServer();
+        new CommandExecutor().run();
     }
 
     @Override
     public void sortBytime() throws Exception {
-        queryToServer();
+        new CommandExecutor().run();
     }
 
     /**
@@ -200,7 +211,7 @@ abstract public class ClientCommandsHandlers implements CommandHandlersWithMeeti
      */
     @Override
     public void reverse() throws Exception {
-        queryToServer();
+        new CommandExecutor().run();
     }
 
     /**
@@ -210,7 +221,7 @@ abstract public class ClientCommandsHandlers implements CommandHandlersWithMeeti
      */
     @Override
     public void swap(int a, int b) throws Exception {
-        queryToServer();
+        new CommandExecutor().run();
     }
 
     /**
@@ -218,6 +229,6 @@ abstract public class ClientCommandsHandlers implements CommandHandlersWithMeeti
      */
     @Override
     public void clear() throws Exception {
-        queryToServer();
+        new CommandExecutor().run();
     }
 }
