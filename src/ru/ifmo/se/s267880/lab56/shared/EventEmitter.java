8f6123175aed33ada9c07b7e46985c952cc869a9
Tran Quang Loc <quangloctran99@gmail.com>
2019-04-17 21:52:22 +0300

    Add a simple event listener, emitter system.

---
+++ src/ru/ifmo/se/s267880/lab56/shared/EventEmitter.java
@@ -0,0 +1,27 @@
+package ru.ifmo.se.s267880.lab56.shared;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class EventEmitter <T> {
+    private List<Consumer<T>> listeners = new LinkedList<>();
+    public EventEmitter() {}
+
+    public Consumer<T> listen(Consumer<T> listener) {
+        listeners.add(listener);
+        return listener;
+    }
+
+    public boolean removeListener(Consumer<T> listener) {
+        return listeners.remove(listener);
+    }
+
+    public void emit(T message) {
+        listeners.forEach(l -> l.accept(message));
+    }
+
+    public void clear() {
+        listeners.clear();
+    }
+}
