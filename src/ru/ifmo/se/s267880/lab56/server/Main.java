f576f02f554c6bc20c021233517f245cb021e109
Tran Quang Loc <quangloctran99@gmail.com>
2019-04-21 00:43:14 +0300

    Move CommandController and Handlers from Main to QueryHandlerThread.

--- src/ru/ifmo/se/s267880/lab56/server/Main.java
+++ src/ru/ifmo/se/s267880/lab56/server/Main.java
@@ -1,46 +1,17 @@
 package ru.ifmo.se.s267880.lab56.server;
 
 import ru.ifmo.se.s267880.lab56.shared.*;
-import ru.ifmo.se.s267880.lab56.shared.commandsController.CommandController;
-import ru.ifmo.se.s267880.lab56.shared.commandsController.helper.ReflectionCommandHandlerGenerator;
-import ru.ifmo.se.s267880.lab56.shared.communication.CommandExecuteRespond;
-import ru.ifmo.se.s267880.lab56.shared.communication.CommandExecuteRespondStatus;
 
 import java.io.*;
 import java.net.ServerSocket;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        String savedFileName = null;
-        if (args.length > 0) {
-            savedFileName = args[0];
-        }
-
-        ServerCommandsHandlers mm = new ServerCommandsHandlers(Collections.synchronizedList(new LinkedList<Meeting>()));
-        mm.load(savedFileName, HandlerCallback.ofErrorHandler(e -> {
-            System.err.println(e.getMessage());
-            System.exit(0);
-        }));
-
-        CommandController cc = new CommandController();
-        ReflectionCommandHandlerGenerator.generate(SharedCommandHandlers.class, mm, new ServerInputPreprocessor())
-                .forEach(cc::addCommand);
-
         try (ServerSocket ss = new ServerSocket(Config.COMMAND_EXECUTION_PORT)) {
             System.out.println("Server connected at " + ss.getLocalPort());
             while (true) {
                 try {
-                    new QueryHandlerThread(ss.accept(), cc) {
-                        @Override
-                        CommandExecuteRespond generateResult(CommandExecuteRespondStatus status, Serializable result) {
-                            LinkedList<Meeting> clonedCollection = new LinkedList<>(mm.getCollection());
-                            clonedCollection.sort(Comparator.comparing(Meeting::getName));
-                            return new CommandExecuteRespond(status, result, clonedCollection);
-                        }
-                    }.start();
+                    new QueryHandlerThread(ss.accept()).start();
                 } catch (IOException e) {
                     System.err.println("Cannot run thread due to IOException: " + e.getMessage());
                     e.printStackTrace();
--- src/ru/ifmo/se/s267880/lab56/server/QueryHandlerThread.java
+++ src/ru/ifmo/se/s267880/lab56/server/QueryHandlerThread.java
@@ -1,16 +1,23 @@
 package ru.ifmo.se.s267880.lab56.server;
 
 import ru.ifmo.se.s267880.lab56.shared.HandlerCallback;
+import ru.ifmo.se.s267880.lab56.shared.Meeting;
+import ru.ifmo.se.s267880.lab56.shared.SharedCommandHandlers;
 import ru.ifmo.se.s267880.lab56.shared.commandsController.CommandController;
+import ru.ifmo.se.s267880.lab56.shared.commandsController.helper.ReflectionCommandHandlerGenerator;
 import ru.ifmo.se.s267880.lab56.shared.communication.*;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.function.Consumer;
 
-abstract public class QueryHandlerThread extends Thread {
+public class QueryHandlerThread extends Thread {
     private Socket client;
-    private CommandController cc;
+    private ServerCommandsHandlers commandsHandlers;
+    private CommandController commandController;
     private Sender messageToClientSender;
     private Receiver messageFromClientReceiver;
 
@@ -18,19 +25,26 @@ abstract public class QueryHandlerThread extends Thread {
         return client;
     }
 
-    public QueryHandlerThread(Socket socket, CommandController cc) {
+    public QueryHandlerThread(Socket socket) {
         System.out.printf("Connected to client %s!\n", socket.getInetAddress());
         this.client = socket;
         messageToClientSender = Sender.fromSocket(socket);
         messageFromClientReceiver = Receiver.fromSocket(socket);
-        this.cc = cc;
+
+        commandsHandlers = new ServerCommandsHandlers(Collections.synchronizedList(new LinkedList<>()));
+        commandController = new CommandController();
+        ReflectionCommandHandlerGenerator.generate(SharedCommandHandlers.class, commandsHandlers, new ServerInputPreprocessor())
+                .forEach(commandController::addCommand);
     }
 
     @Override
     public void run() {
         try {
             CommandExecuteRequest qr = messageFromClientReceiver.receiveWithStream();
-            cc.execute(qr.getCommandName(), qr.getParameters(), new HandlerCallback<>(this::onCommandSuccessfulExecuted, this::onErrorWhenExecutingCommand));
+            commandController.execute(qr.getCommandName(), qr.getParameters(), new HandlerCallback<>(
+                    this::onCommandSuccessfulExecuted,
+                    this::onErrorWhenExecutingCommand
+            ));
         } catch (IOException | ClassNotFoundException e) {
             onDisconnectedToClient(new CommunicationIOException("Cannot read data sent from client.", e));
         }
@@ -58,5 +72,9 @@ abstract public class QueryHandlerThread extends Thread {
         System.out.printf("Disconnected to client %s.\n", client.getInetAddress());
     }
 
-    abstract CommandExecuteRespond generateResult(CommandExecuteRespondStatus status, Serializable result);
+    private CommandExecuteRespond generateResult(CommandExecuteRespondStatus status, Serializable result) {
+        LinkedList<Meeting> clonedCollection = new LinkedList<>(commandsHandlers.getCollection());
+        clonedCollection.sort(Comparator.comparing(Meeting::getName));
+        return new CommandExecuteRespond(status, result, clonedCollection);
+    }
 }
