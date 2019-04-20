372c43df4b9dec9f6de0aea6d83f9f820f73eb1a
Tran Quang Loc <quangloctran99@gmail.com>
2019-04-21 00:25:46 +0300

    Move removeGSONNonExecutablePrefix to Main.

--- src/ru/ifmo/se/s267880/lab56/client/Main.java
+++ src/ru/ifmo/se/s267880/lab56/client/Main.java
@@ -1,5 +1,6 @@
 package ru.ifmo.se.s267880.lab56.client;
 
+import com.google.gson.stream.JsonReader;
 import ru.ifmo.se.s267880.lab56.client.repl.MainLoop;
 import ru.ifmo.se.s267880.lab56.client.repl.input.DefaultUserInputArgumentParser;
 import ru.ifmo.se.s267880.lab56.client.repl.input.JsonUserInputArgumentParser;
@@ -11,10 +12,13 @@ import ru.ifmo.se.s267880.lab56.shared.commandsController.CommandController;
 import ru.ifmo.se.s267880.lab56.shared.commandsController.CommandHandler;
 import ru.ifmo.se.s267880.lab56.shared.commandsController.helper.ReflectionCommandHandlerGenerator;
 import ru.ifmo.se.s267880.lab56.shared.communication.*;
+import sun.misc.Unsafe;
 
 import static ru.ifmo.se.s267880.lab56.client.UserInputHelper.*;
 
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.UnresolvedAddressException;
@@ -26,6 +30,7 @@ import java.util.Arrays;
 public class Main {
     public static InetSocketAddress address = null;
     public static void main(String[] args) {
+        removeGSONNonExecutablePrefix();
         printAwesomeASCIITitle();
 
         if (address == null) {
@@ -175,5 +180,72 @@ public class Main {
         Arrays.stream(helps).forEach(System.out::println);
     }
 
+    /**
+     * A method to remove <code>JsonParser.NON_EXECUTE_PREFIX</code>'s content.
+     *
+     * <p>This method is a hack.</p>
+     *
+     * <h6>The problem.</h6>
+     * <p>
+     *     My task asks me to implements some commands, and one of them has 2 type of argument: a json object and a number.
+     *     We all know that a raw number is also a valid Json object, so all I need is just read the Json object.
+     *     But Google is so smart: when reading a number with 1 digit, Gson tries to read 4 more in order to find out whether
+     *     or not the Json string contains a string called
+     *     <a href="https://google.github.io/gson/apidocs/com/google/gson/stream/JsonReader.html#nonexecuteprefix">
+     *         "non execute prefix"
+     *     </a> for security reason. So in order to execute the current command, the user need to enter 3 more character,
+     *     (because they already hit enter, an '\n' is read).
+     * </p>
+     * <p>
+     *     Of course the easiest solution for the above problem is to check if the first non-whitespace character is a
+     *     number or is an load brace. But the second problem is that I want the command to be execute with more than
+     *     one arguments. For example we want an command to insert an object into a specific place in a collection,
+     *     so we need 2 arguments. And the problem now, is when using JsonReader or JsonParser, for the sake of the speed,
+     *     the reader will try to read all the lines, into its buffer, so there is "almost" no way to get the next data
+     *     in the same line after an argument is read, unless, using the JsonParser itself to read more.
+     * </p>
+     * <p>
+     *     And also because of its fancy function, gson can read unquoted string, so I also use JsonParser as both of my
+     *     reader: the command reader and the input reader.
+     * </p>
+     * <h6>The solution.</h6>
+     * <p>
+     *     Because this is a small project, and currently not related to the internet, so this method was made so it can
+     *     remove the content of the "non execute prefix" in the JsonReader object. That way, the commands is more
+     *     freely to read the user input, and JsonParser is still safe and not malfunction.
+     * </p>
+     * <p>To archive this, reflection is used with a little help of online code that suspend the looger (so it will look nicer).</p>
+     *
+     */
+    private static boolean removeGSONNonExecutablePrefix() {
+        /*
+         * The code for turning of the logger can be found here:
+         * https://stackoverflow.com/questions/46454995/how-to-hide-warning-illegal-reflective-access-in-java-9-without-jvm-argument
+         */
+        try {
+            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            Unsafe u = (Unsafe) theUnsafe.get(null);
+
+            Class cls = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field logger = cls.getDeclaredField("logger");
+            u.putObjectVolatile(cls, u.staticFieldOffset(logger), null);
+        } catch (Exception e) {
+        }
+
+        try {
+            Field nonExecutePrefixField = JsonReader.class.getDeclaredField("NON_EXECUTE_PREFIX");
+            nonExecutePrefixField.setAccessible(true);
+
+            Field modifier = Field.class.getDeclaredField("modifiers");
+            modifier.setAccessible(true);
+
+            modifier.set(nonExecutePrefixField,nonExecutePrefixField.getModifiers() & ~Modifier.FINAL);
+            nonExecutePrefixField.set(null, new char[]{});
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            return false;
+        }
+        return true;
+    }
 }
 
