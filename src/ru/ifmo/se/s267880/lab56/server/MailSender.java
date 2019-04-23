f65b2e60684a8c03a4dd0d45f7fb6f7f86ac3f49
Tran Quang Loc <quangloctran99@gmail.com>
2019-04-23 07:09:40 +0300

    Add MailSender and config server probject with external config file.

---
+++ src/ru/ifmo/se/s267880/lab56/server/MailSender.java
@@ -0,0 +1,66 @@
+package ru.ifmo.se.s267880.lab56.server;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+public class MailSender {
+    private String from;
+    private String username;
+    private String password;
+    private Session messageSession;
+
+    public static MailSender fromJson(JsonElement elm) {
+        JsonObject obj = elm.getAsJsonObject();
+        MailSender res = new MailSender();
+        res.setFrom(obj.get("email-address").getAsString());
+        res.setUsername(obj.get("username").getAsString());
+        res.setPassword(obj.get("password").getAsString());
+        Properties props = new Properties();
+        for (Map.Entry<String, JsonElement> entry : obj.getAsJsonObject("properties").entrySet()) {
+            props.put(entry.getKey(), entry.getValue().getAsString());
+        }
+        res.setMessageSession(Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(res.username, res.password);
+            }
+        }));
+        return res;
+    }
+
+    public synchronized void sendHTMLMail(String to, String subject, String body) throws MessagingException {
+        MimeMessage msg = new MimeMessage(messageSession);
+        msg.setFrom(new InternetAddress(from));
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+        msg.setSubject(subject, "UTF-8");
+        msg.setText(body, "UTF-8");
+        Transport.send(msg);
+    }
+
+    public synchronized void setFrom(String from) {
+        this.from = from;
+    }
+
+    public synchronized void setMessageSession(Session messageSession) {
+        this.messageSession = messageSession;
+    }
+
+    public synchronized void setPassword(String password) {
+        this.password = password;
+    }
+
+    public synchronized void setUsername(String username) {
+        this.username = username;
+    }
+}
--- src/ru/ifmo/se/s267880/lab56/server/Main.java
+++ src/ru/ifmo/se/s267880/lab56/server/Main.java
@@ -1,5 +1,9 @@
 package ru.ifmo.se.s267880.lab56.server;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 import ru.ifmo.se.s267880.lab56.shared.*;
 import ru.ifmo.se.s267880.lab56.shared.communication.UserNotification;
 
@@ -15,16 +19,30 @@ public class Main {
     private static EventEmitter<UserNotification> onNotification = new EventEmitter<>();
     private static Connection databaseConnection;
     private static UserStatePool userStatePool;
+    private static MailSender mailSender;
     public static void main(String[] args) {
-        try {
-            databaseConnection = initDataBase();
-            userStatePool = new UserStatePool(databaseConnection);
-        } catch (SQLException e) {
-            System.err.println("Error while initialization data base: " + e.getMessage());
-            System.exit(1);
+        try (FileInputStream configFile = new FileInputStream("config.json")) {
+            JsonElement elm = new JsonParser().parse(new InputStreamReader(configFile));
+            mailSender = MailSender.fromJson(elm.getAsJsonObject().getAsJsonObject("mail"));
+            try {
+                JsonObject dbConfig = elm.getAsJsonObject().getAsJsonObject("database");
+                databaseConnection = initDataBase(
+                        dbConfig.get("url").getAsString(),
+                        dbConfig.get("user").getAsString(),
+                        dbConfig.get("password").getAsString()
+                );
+                userStatePool = new UserStatePool(databaseConnection);
+            } catch (SQLException e) {
+                System.err.println("Error while initialization data base: " + e.getMessage());
+                System.exit(1);
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+                System.exit(1);
+            }
         } catch (IOException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
+            System.err.println("Problem while reading config.json: " + e.getMessage());
+            System.err.println("If you have not added config.json file yet, please add it for program configuration.");
+            System.exit(0);
         }
 
         QueryHandlerThread.Builder handlerThreadBuiler = new QueryHandlerThread.Builder();
@@ -49,10 +67,8 @@ public class Main {
         }
     }
 
-    private static Connection initDataBase() throws SQLException, IOException {
-        Connection res = DriverManager.getConnection(
-            ServerConfig.DB_URL, ServerConfig.DB_USER, ServerConfig.DB_PASSWORD
-        );
+    private static Connection initDataBase(String url, String user, String password) throws SQLException, IOException {
+        Connection res = DriverManager.getConnection(url, user, password);
         InputStream is = Class.class.getResourceAsStream("/ru/ifmo/se/s267880/lab56/server/res/create_tables.sql");
         if (is == null) throw new IOException("Cannot load script from resource.");
         BufferedReader in = new BufferedReader(new InputStreamReader(is));
